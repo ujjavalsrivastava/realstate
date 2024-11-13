7d346ed04b97af2b3d127ab7ec38591fd282cb9f
ujjval <ujjval000@gmail.com>
2024-11-13 11:20:48 +0530

    upimage

--- resources/views/ajax/chatuserlist.blade.php
+++ resources/views/ajax/chatuserlist.blade.php
@@ -9,21 +9,21 @@
 
                     @endif
                 </div>
-                <div class="pro-name">
+                <div class="pro-name" style="width:48%">
                @php
 
                $count = UserUnseenMsg($row->id);
 
                @endphp
                     <div class="emp-name">
-                        <div class="">
+                        <div class="" style="display: inline-block;float: left;">
                             <span>{{ucfirst($row->name)}}</span>
                         </div>
-                        <div class="">
+                        <div class="" style="display: inline-block;float: right;">
                             <span class="msgCount">{{$count}}</span>
                         </div>
                     </div>
-                    <div class="bottom-text"><span>{{ucfirst($row->type)}}</span></div>
+                    <div class="bottom-text" style="position: relative;top: 17px;right: 26px;"><span>{{ucfirst($row->type)}}</span></div>
                 </div>
             </div>
             @endforeach
