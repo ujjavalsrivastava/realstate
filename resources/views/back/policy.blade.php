8fb77b19fcfab072c6d82ca1b116cc7bffe0fab5
ujjvalsrivastava <ujjval000@gmail.com>
2025-01-24 20:38:38 +0530

    upda

--- resources/views/back/policy.blade.php
+++ resources/views/back/policy.blade.php
@@ -23,44 +23,75 @@
             <div class="pftext">
                <!-- <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laborum odio id voluptatibus incidunt cum? Atque quasi eum debitis optio ab. Esse itaque officiis tempora possimus odio rerum aperiam ratione, sunt. Lorem ipsum dolor sit amet, consectetur adipisicing elit sunt.</p>
                   <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laborum odio id voluptatibus incidunt cum? Atque quasi eum debitis optio ab. Esse itaque officiis tempora possimus odio rerum aperiam ratione, sunt. Lorem ipsum dolor sit amet, consectetur adipisicing elit sunt.</p> -->
-               <p>Welcome to !Alpha Land! We are a real estate information service connecting people in the real estate market. Alphaland.in and our subsidiaries and affiliates (collectively " Alphaland.in ") provide you with access to a variety of services, including but not limited to the www. Alphaland.in website (the "Website") and Alpha Land mobile applications and all the products and services available through our website (collectively the "Services"). The Website is owned and operated by Locon Solutions Private Limited, a company incorporated under the laws of India.
-Our Privacy Policy is incorporated as part of the Alphaland.in Terms of Use. Please read this Privacy Policy and our Terms of Use carefully before using our Services. By using Alphaland.in Services, you agree to the provisions of this Privacy Policy. If you do not want to be bound by our Privacy Policy, please do not use our Services.
-Why We Collect Information from You
-Alphaland.in is a real estate information service. To provide our Services, we use the information we collect to:
-• Help you find the most relevant information for your situation by customizing our Services to optimize your experience;
-• Keep you connected with Alphaland.in across the internet and update you with residential real estate-related news and information; and
-• Put you in touch with the right people to sell, buy, rent, or research residential real estate, obtain a mortgage, and to optimize the information we share with those parties to initiate a productive and efficient relationship.
- 
-Our goal in this policy is to be completely transparent about the data we collect, why we collect it, and how we use it, in order to make finding your new home a pleasant and fun experience!
-To Whom Does This Policy Apply
-Some parts of this Policy are directed at specific types of people. We use some special terminology to refer to them, which we define below. This Policy applies to everyone who uses our Services, but sometimes we direct parts of it toward particular groups of users.
-Users: "Users" are everyone who uses our Services.
-Real Estate Professionals (REPs): "Real Estate Professionals," or "REPs" are users who have created accounts (free or paid) with us to sell or rent real estate, participate in our advertising services, and participate in other Services that we direct toward the professional real estate community. REPs include landlords, agents, developers, institutional property consultants, mortgage professionals and subscribers. We send leads to REPs on your behalf, as described in this Policy.
-What Information Do We Collect, and Where Do We Get It
-We Collect Some Information From All Visitors to Our Website: Like many online services, we use technologies like session and persistent cookies, web beacons (tiny image files on web pages that communicate information about the page viewer to the beacon owner), log data, and third-party analytics services to collect and analyze information about all users of our Services This includes things like User search preferences, interaction with ads on our Webite, and location.
-Our servers automatically record information ("Log Data") created by your use of the Services. Log Data may include information such as your IP address, browser type, operating system, the referring web page, pages visited, location, your mobile number, your mobile carrier, your email address, device, search terms, and cookie information. We receive Log Data when you interact with our Services, for example, when you visit our websites, sign into our Services, or interact with our email notifications. We use Log Data to provide our Services and to measure, customize, and improve them, as well as collect and use them in the aggregate. This Policy does not apply to, nor do we take any responsibility for any information that is collected by any third party either using the Website or through any links on the Website or through any of the advertisements.
-We collect some personal information like name, contact details, mobile number, email address, profile images/photos/pictures which you have provided us during the account creation / registration process or while interacting with any of our services to ensure a smoother user experience and to tailor our services to your preferences and interests.
-Cookies are small data files that we transfer to your computer. We use "session" cookies to keep you logged in while you use our Services, to better understand how you interact with our Services, and to monitor aggregate usage and web traffic information on our Services. We use "persistent" cookies to recognize you each time you return to our Services. For example, we create a persistent cookie that includes some basic information about you, like your most recent search and whether an agent responded to a query you sent using Alphaland.in Services. We use this persistent cookie to remember your preferences and, if you create an account, to make your user experience consistent after you register.
-Most internet browsers automatically accept cookies, but you can change your settings or use third-party tools to refuse cookies or prompt you before accepting cookies from the websites you visit. You can also use your browser settings or other tools to delete cookies you already have. Please be aware that some parts of our Services may not work for you if you disable cookies.
-Information We Collect from REPs: If you create a REP account with us, whether as a landlord, agent, developer or subscriber we will ask you to provide certain information in order to set up your account. We ask all REPs to provide a name, a company (if available), a phone number, and an email address. If you create a paid REP account and become a subscriber, we require you to submit all of the information required from landlords and agents as well as a description of your campaign. We will also collect financial data like credit card information and billing addresses from subscribers, so that we can handle billing. If you are a subscriber, a Alphaland.in representative may call you to verify your information.
-Additional Information About You that We Obtain from Third Parties: We may sometimes obtain information about you from third parties and use it to help agents serve you better. For example, our partners may help us provide predictions of your income range and how likely you may be to purchase a home. We will share this augmented data only with selected REPs (subscribers) who you choose to share information with by contacting them through the Alpha Land Services.
-Location Data: If you provide location information (like a ZIP code, street address, or even just a city) during the registration process or at any other time via your account settings, we will store that information and associate it with your account.
-We may use and store information about your location to provide localized features, to analyze our Services with the help of third-party analytics services, to provide you with relevant advertising and marketing materials, and otherwise to improve and customize our Services.
+               <p>
+               This privacy policy sets out how Alpha Land uses and protects any information that you give Alpha Land when you visit their website and/or agree to purchase from them.
+Alpha Land is committed to ensuring that your privacy is protected. Should we ask you to provide certain information by which you can be identified when using this website, and then you can be assured that it will only be used in accordance with this privacy statement.
+Alpha Land may change this policy from time to time by updating this page. You should check this page from time to time to ensure that you adhere to these changes.
+               </p>
+               <p><b>We may collect the following information:
+                  </b></p>
+               <p>
+               • Name
 
                </p>
-               <p><b>How We Share Your Information               </b></p>
-               <p>As you use, access and navigate our Website, we collect various information, which may either be personally identifiable information or information that is not personally identifiable. This information is classified as:</p>
-<p>• “Personal Information” which means personally identifiable information like your name, your address, your age, your email address and your phone number.</p>
-<p>• “Sensitive Personal Information” which means your financial information, your passwords, any biometric, medical or health related information.</p>
-<p>• “Other Information” which means all other information other than “Personal Information” or “Sensitive Personal Information.” For example, the nature of device information, your usage patterns, your usage preferences, logs which are created when you access and use the Website, local caches on your browser, your location, ip address and the like. These are just examples and the list is by no means exhaustive.
-Any personal information will be used to allow you to login to your account or to resolve specific service issues, inform you of our new services or features and to communicate with you in relation to your use of the Website. Any other information we collect will be used for-</p>
-<p>• our business purposes and this may include the viewing or advertising of residential or commercial real estate, rental properties or to use associated services;</p>
-<p>• analytical purposes, data usage;</p>
-<p>• improving the Website, or your user experience; </p> 
-<p>• providing targeted advertisements to you.</p>
-We will share all information we collect from you, including the personal information with any third party if we in good faith believe it is required for the purposes of our compliance with any applicable law, regulation, legal process or enforceable governmental request/action or any manner required for the purposes of protecting our rights or interests.
-With respect to Ad Servers: To try and bring you offers that are of interest to you, we have relationships with other companies that we allow to place ads on our Web pages. As a result of your visit to our Website, ad server companies may collect information such as your domain type, your IP address and clickstream information.
- 
+               • Contact information including email address
+
+               <p>
+               • Demographic information such as postcode, preferences and interests, if required
+
+
+</p>
+
+<p>
+• Other information relevant to customer surveys and/or of
+</p>
+
+<p><b>What we do with the information we gather
+
+   </b></p>
+<p>•
+We require this information to understand your needs and provide you with a better service, and in particular for the following reasons:
+
+</p>
+
+<p>
+• Internal record keeping.
+• We may use the information to improve our products and services.
+• We may periodically send promotional emails about new products, special offers or other information which we think you may find interesting using the email address which you have provided.
+• From time to time, we may also use your information to contact you for market research purposes. We may contact you by email, phone, fax or mail. We may use the information to customise the website according to your interests.
+We are committed to ensuring that your information is secure. In order to prevent unauthorised access or disclosure we have put in suitable measures.
+
+</p>
+
+<p>•
+   <b>
+How we use cookies
+</b>
+
+</p>
+
+<p>•
+A cookie is a small file which asks permission to be placed on your computer's hard drive. Once you agree, the file is added and the cookie helps analyze web traffic or lets you know when you visit a particular site. Cookies allow web applications to respond to you as an individual. The web application can tailor its operations to your needs, likes and dislikes by gathering and remembering information about your preferences.
+We use traffic log cookies to identify which pages are being used. This helps us analyze data about webpage traffic and improve our website in order to tailor it to customer needs. We only use this information for statistical analysis purposes and then the data is removed from the system.
+Overall, cookies help us provide you with a better website, by enabling us to monitor which pages you find useful and which you do not. A cookie in no way gives us access to your computer or any information about you, other than the data you choose to share with us.
+You can choose to accept or decline cookies. Most web browsers automatically accept cookies, but you can usually modify your browser setting to decline cookies if you prefer. This may prevent you from taking full advantage of the website.
+
+</p>
+
+<p>•
+   <b>
+   Controlling your personal information
+
+</b>
+
+</p>
+
+<p>•
+You may choose to restrict the collection or use of your personal information in the following ways:
+• whenever you are asked to fill in a form on the website, look for the box that you can click to indicate that you do not want the information to be used by anybody for direct marketing purposes
+• if you have previously agreed to us using your personal information for direct marketing purposes, you may change your mind at any time by writing to or emailing us at
+We will not sell, distribute or lease your personal information to third parties unless we have your permission or are required by law to do so. We may use your personal information to send you promotional information about third parties which we think you may find interesting if you tell us that you wish this to happen.
+If you believe that any information we are holding on you is incorrect or incomplete, please write to Dandupur, Chandmari, Ring road, Varanasi, Uttar Pradesh Varanasi UTTAR PRADESH 221201 . or contact us at or as soon as possible. We will promptly correct any information found to be incorrect.
 </p>
             </div>
             <!-- <div class="box bg-2">
--- resources/views/back/termsconditions.blade.php
+++ resources/views/back/termsconditions.blade.php
@@ -24,32 +24,50 @@
                <!-- <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laborum odio id voluptatibus incidunt cum? Atque quasi eum debitis optio ab. Esse itaque officiis tempora possimus odio rerum aperiam ratione, sunt. Lorem ipsum dolor sit amet, consectetur adipisicing elit sunt.</p>
                   <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laborum odio id voluptatibus incidunt cum? Atque quasi eum debitis optio ab. Esse itaque officiis tempora possimus odio rerum aperiam ratione, sunt. Lorem ipsum dolor sit amet, consectetur adipisicing elit sunt.</p> -->
                <p>
-               terms-conditionsterms-conditionsThese terms of use ("Terms") cons2tute a legally binding agreement between you and Alpha Land) regarding the use of the web applica2on/ website www.alphaland.in (the "Site") or the domain thereof and any service offered or deemed to be offered by the Company including but not limited to delivery of content via the Site, any mobile or internet connected device or otherwise (the "the Service").</p>
-           <p> Your use of the Site and services and tools are governed by the following Terms & Condi2ons which are incorporated herein. By mere use of the Site, you shall be contrac2ng with Alpha Land Realty Services Limited, the owner of the PlaJorm. These terms and condi2ons including the policies cons2tute your binding obliga2ons, with Alpha Land Therefore, it is impera2ve that before using the plaJorm you acquaint yourself and understand the applicability and consequen2al applica2on of these terms & condi2ons.</p>
+               For the purpose of these Terms and Conditions, The term "we", "us", "our" used anywhere on this page shall mean Alpha Land, whose registered/operational office is Dandupur, Chandmari, Ring road, Varanasi, Uttar Pradesh Varanasi UTTAR PRADESH 221201 . "you", “your”, "user", “visitor” shall mean any natural or legal person who is visiting our website and/or agreed to purchase from us.
+
+            </p>
+           <p> <b>
+           Your use of the website and/or purchase from us are governed by following Terms and Conditions: </b></p>
           
          <p> 
-         When You use any of the services provided by Us through the PlaJorm, including but not limited to, (e.g. Product Reviews, Seller Reviews), You will be subject to the rules, guidelines, policies, terms, and condi2ons applicable to such service, and they shall be deemed to be incorporated into these Terms and shall be considered as part and parcel of these Terms.</p>
+         • The content of the pages of this website is subject to change without notice.</p>
        
-      <p> Alpha Land may amend/modify these terms and condi2ons at any 2me, and such modifica2ons shall be effec2ve immediately upon pos2ng of the modified terms and condi2ons on Alpha Land. You may review the modified terms and condi2ons periodically to be aware of such modifica2ons and your con2nued access or use of Alpha Land, shall be deemed conclusive proof of your acceptance of these terms and condi2ons, as amended/modified from 2me to 2me Alpha Land reserves the right to suspend the opera2ons for support or technical upgrada2on, maintenance work, in order to update the content or for any other reason without in2ma2ng any user in advance.</p>
+      <p> 
+      • Neither we nor any third parties provide any warranty or guarantee as to the accuracy, timeliness, performance, completeness or suitability of the information and materials found or offered on this website for any particular purpose. You acknowledge that such information and materials may contain inaccuracies or errors and we expressly exclude liability for any such inaccuracies or errors to the fullest extent permitted by law.
+      </p>
      
-   <p> Defined Terms:
-   Unless otherwise specified, the capitalized words shall have the meanings as defined herein below: "Agreement" shall mean and include the completed applica2on form, its aSachment(s) and the terms and condi2ons stated herein.
-"Alphaland.in" is defined as the internet website or mobile applica2on of the Company at www.alphaland.in
-"My Subscrip2ons" contains 2me to 2me informa2on and descrip2on of the Services for the User provided by the Company in wri2ng or contained in the website Alphaland.in.
-Registra2on Data" is the database of all the par2culars and informa2on supplied by the User on ini2al applica2on and subscrip2on, including but without limi2ng to the User's name, telephone number, mailing address, account and email address.
-"User" will includes everyone who is a Subscriber/Adver2ser(s) and Browser/Visitor(s) either as an individual or corporate subscriber for the Services and the signatory, whose par2culars are contained in the applica2on form and includes his successors and permiSed assignees. "User" or "You" also includes any person who access or avail the Services provided by the Company for the purpose of hos2ng, publishing, sharing, lis2ng, transac2ng, viewing, displaying or uploading informa2on or views and includes other persons jointly par2cipa2ng in using the Services provided by the Company.
-Words referring to masculine include the feminine and the singular include the plural and vice versa as the context admits or requires; and Words impor2ng persons includes individuals, bodies corporate and unincorporated.
-Term:
-These Terms shall con2nue to form a valid and binding contract between the Par2es and shall con2nue to be in full force and effect 2ll the User con2nues to access the Site and avails the Services provided by the Company.
-Services:
-Company provides a number of internet-based services through its plaJorm and shall include:
-Pos2ng User profile or lis2ng for the purpose of sale/rental of property, and related property services etc.
-Find a property through Alphaland.in and its internet links.
-Place a print adver2sement in any of the group publica2ons through the www.Alphaland.in site. Post adver2sements on Alphaland.in
-Send adver2sements and promo2onal messages through emails and messages.
-The Services can be purchased through various methods of payments offered. The purchase of Services shall be addi2onally governed by specific policies of sale, like subscrip2on fees, payment and Refund policy, cancella2on policy etc.
-Eligibility:
-You hereby represent and warrant to the Company that you are at least eighteen (18) years of age or above and are competent to enter into a valid and binding contract apart from being capable of entering, performing and adhering to these Terms & Condi2ons. While individuals under the age of 18 may u2lize the Service of the site, they shall do so only with the involvement & guidance of their parents and / or legal guardians, under such Parent /Legal guardian's registered account. You agree to register prior to uploading any content and / or comment and any other use or services of this site and provide your details including but not limited to complete name, age, email address, residen2al address, and contact number. </p>
+   <p> 
+   • Your use of any information or materials on our website and/or product pages is entirely at your own risk, for which we shall not be liable. It shall be your own responsibility to ensure that any products, services or information available through our website and/or product pages meet your specific requirements.
+ </p>
+ <p> 
+ • Our website contains material which is owned by or licensed to us. This material includes, but are not limited to, the design, layout, look, appearance and graphics. Reproduction is prohibited other than in accordance with the copyright notice, which forms part of these terms and conditions.
+ </p>
+
+ <p> 
+
+ • All trademarks reproduced in our website which are not the property of, or licensed to, the operator are acknowledged on the website.
+• Unauthorized use of information provided by us shall give rise to a claim for damages and/or be a criminal offense.
+
+</p>
+
+<p> 
+• From time to time our website may also include links to other websites. These links are provided for your convenience to provide further information.
+• You may not create a link to our website from another website or document without Alpha Land’s prior written consent.
+
+      </p>
+     
+      <p> 
+      • Any dispute arising out of use of our website and/or purchase with us and/or any engagement with us is subject to the laws of India .
+
+      </p>
+     
+      <p> 
+      • We, shall be under no liability whatsoever in respect of any loss or damage arising directly or indirectly out of the decline of authorization for any Transaction, on Account of the Cardholder having exceeded the preset limit mutually agreed by us with our acquiring bank from time to time.
+
+      </p>
+     
+     
    </div>
             <!-- <div class="box bg-2">
                <a href="about.html" class="text-center button button--moema button--text-thick button--text-upper button--size-s">read More</a>
